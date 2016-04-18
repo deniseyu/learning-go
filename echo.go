d177eceb4bba6769815c114f33a5f399e59a7980
Denise Yu <yu.denise.d@gmail.com>
2016-04-18 22:12:35 +0100

    chapter 1.2

--- echo.go
+++ echo.go
@@ -4,20 +4,18 @@ import (
   "fmt"
   "os"
   "strings"
-  // "strconv"
+  "strconv"
 )
 
 func main()  {
 // 1.1
-  words := strings.Join(os.Args[0:], " ")
-
-  fmt.Println(words)
+  // words := strings.Join(os.Args[0:], " ")
+  //
+  // fmt.Println(words)
 
 // 1.2
-  // for i := 0; i < len(os.Args); i++ {
-  //   argWithIndex := strings.Join([]string{strconv.Itoa(i), os.Args[i]}, " ")
-  //   // fmt.Println(i)
-  //   // fmt.Println(os.Args[i])
-  //   fmt.Println(argWithIndex)
-  // }
+  for i := 0; i < len(os.Args); i++ {
+    argWithIndex := strings.Join([]string{strconv.Itoa(i), os.Args[i]}, " ")
+    fmt.Println(argWithIndex)
+  }
 }
